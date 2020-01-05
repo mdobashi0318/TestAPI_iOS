8cc90685d8245e4fa69a608b4243b9f09d9d5113
Masaharu Dobashi <dobashi.masaharu@gmail.com>
2020-01-05 14:38:07 +0900

    storyboard削除等

--- TestAPI/ViewController.swift
+++ TestAPI/ViewController.swift
@@ -8,16 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UITableViewController {
 
     /// リクエストするURL
     let url = URL(string: "http://localhost:3000/api/v1/users")
     
-    /// テーブルビュー
-    var tableView: UITableView!
-    
-    /// テーブルビューを上からスワイプしたとき
-    let refreshControl = UIRefreshControl()
     
     /// ユーザー名を格納
     var usersModel: [[String: Any]]? {
@@ -34,14 +29,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        tableView = UITableView(frame: UIScreen.main.bounds, style: .grouped)
+        tableView = UITableView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height), style: .grouped)
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         tableView.dataSource = self
         tableView.delegate = self
-        tableView.refreshControl = refreshControl
-        view.addSubview(tableView)
-        
-        refreshControl.addTarget(self, action: #selector(ViewController.refresh(sender:)), for: .valueChanged)
+        tableView.refreshControl?.addTarget(self, action: #selector(ViewController.refresh(sender:)), for: .valueChanged)
+
         
     }
     
@@ -53,9 +46,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     
     
+    
+    
     // MARK: UITableViewDelegate, UITableViewDataSource
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell")!
         cell.textLabel?.text = usersModel?[indexPath.row]["name"] as? String
         
@@ -63,10 +58,21 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-                postRequest()
-//        putRequest(at: indexPath.row)
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//                postRequest()
+        putRequest(at: indexPath.row)
     }
+    
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if usersModel == nil {
+            return 1
+        }
+        
+        return usersModel!.count
+    }
+    
     
     
     @objc func refresh(sender: UIRefreshControl) {
@@ -113,7 +119,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         
         let params:[String:Any] = [
-            "user":["name":"新ユーザ", "activeflag": "1"]
+            "user":["name":"新ユーザ"]
         ]
         
         
@@ -143,7 +149,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
            
            let params:[String:Any] = [
-               "user":["name":"更新名", "activeflag": "1"]
+               "user":["name":"更新名"]
                
            ]
            
@@ -162,14 +168,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                print("エラー")
            }
        }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if usersModel == nil {
-            return 1
-        }
-        
-        return usersModel!.count
-    }
+
     
 
 }
